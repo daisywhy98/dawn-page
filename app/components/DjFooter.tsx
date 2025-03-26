54b5c28aff451f805277b607095a56c35fa93e1e
daisy.wu <daisy.wu@emallcoder.com>
2025-03-26 17:02:07 -0400

    add footer

---
+++ app/components/DjFooter.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+const DjHeader = () => {
+    return (
+        <div className='w-full h-28 p-6 bg-linear-to-r from-green-700 to-green-500' >
+            <div>Welcome to my website</div>
+            <div className='flex flex-row items-center justify-between'>
+                <div>subscribe</div>
+                <div>blog</div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default DjHeader
--- app/components/DjHeader.tsx
+++ app/components/DjHeader.tsx
@@ -5,11 +5,11 @@ import Population from './Population'
 
 const DjHeader = () => {
     return (
-        // <div className='w-full h-28 bg-linear-to-r from-green-700 to-green-500 p-6'>
-        <div className='w-full h-28 p-6 bg-linear-to-r from-green-700 to-green-500' style={{ backgroundImage: "url('/images/rotating-galex-6.gif')", backgroundSize: "100% auto", 
+       
+        <div className='w-full h-28 p-6' style={{ backgroundImage: "url('/images/rotating-galex-6.gif')", backgroundSize: "100% auto", 
             backgroundPosition: "center", 
             backgroundRepeat: "no-repeat"  }}>
-            <div>Welcome to my website</div>
+            <div className="text-3xl font-bold text-green-200">Welcome to Dawn's website</div>
             <div className='flex flex-row items-center justify-between'>
             <CurrentTime></CurrentTime>
             <Population></Population>
