737c0d99e2244f65bac1671126ce8dbd6c729015
daisy.wu <daisy.wu@emallcoder.com>
2025-03-27 15:29:16 -0400

    add footer link

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <DjHeader></DjHeader>
-        <div className="h-[calc(100vh-300px)]">
+        <div className="h-[calc(100vh-336px)]">
           {children}
         </div>
 
